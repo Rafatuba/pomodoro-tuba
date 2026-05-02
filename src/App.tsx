8af6fa546697c0d5a21718c52ae5d88af0f47140
Rafatuba <rafaolly1@gmail.com>
2026-05-02 10:47:13 -0400

    Início projeto Pomodoro Tuba

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Cheguei, Brasil!</h1>
+      <h2>Não contavam com minha astúcia!</h2>
+      <img
+        src="https://static.wikia.nocookie.net/liberproeliis/images/1/1a/JdFcRwL.png/revision/latest?cb=20181210214019&path-prefix=pt-br"
+        alt="chapolin"
+        width={400}
+      />
+    </>
+  );
+}
+
+export default App;
