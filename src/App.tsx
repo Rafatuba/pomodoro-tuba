7e423b5731f135f3ca615144846549e0c7a7e460
Rafatuba <rafaolly1@gmail.com>
2026-05-02 22:22:59 -0400

    Até o componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import "./App.css";
+import { IconStopwatch } from "@tabler/icons-react";
+import { Heading } from "./components/Heading";
+import "./styles/global.css";
+import "./styles/theme.css";
 
 function App() {
   return (
     <>
-      <h1>Cheguei, Brasil!</h1>
-      <h2>Não contavam com minha astúcia!</h2>
+      <Heading>
+        Olá mundo!
+        <button>
+          <IconStopwatch size={36} />
+        </button>
+      </Heading>
+      <p>Não contavam com minha astúcia!</p>
       <img
         src="https://static.wikia.nocookie.net/liberproeliis/images/1/1a/JdFcRwL.png/revision/latest?cb=20181210214019&path-prefix=pt-br"
         alt="chapolin"
